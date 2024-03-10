4f9fdc6290044ae379e8780078f1ee59f8093430
tronghieu <tronghieu0944@gmail.com>
2024-03-10 19:30:40 +0700

    profile

---
+++ app/src/main/java/com/instar/frontend_android/ui/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.instar.frontend_android.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.instar.frontend_android.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

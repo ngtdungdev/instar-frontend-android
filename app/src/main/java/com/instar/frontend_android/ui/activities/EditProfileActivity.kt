4c66c18a9ee7bfaa4d757cac8e1f992ebb3a3ba0
tronghieu <tronghieu0944@gmail.com>
2024-03-18 13:27:33 +0700

    profile

--- app/src/main/java/com/instar/frontend_android/ui/activities/EditProfileActivity.kt
+++ app/src/main/java/com/instar/frontend_android/ui/activities/EditProfileActivity.kt
@@ -3,19 +3,19 @@ package com.instar.frontend_android.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.instar.frontend_android.R
 
-class ProfileActivity : AppCompatActivity() {
-
+class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_edit_profile)
 
-        val btn_editProfile = findViewById<TextView>(R.id.btnEditProfile)
+        val btn_editProfile1 = findViewById<ImageView>(R.id.btn_back)
 
-        btn_editProfile.setOnClickListener {
-            val newPage = Intent(this@ProfileActivity, EditProfileActivity::class.java)
+        btn_editProfile1.setOnClickListener {
+            val newPage = Intent(this@EditProfileActivity, ProfileActivity::class.java)
             startActivity(newPage)
         }
     }

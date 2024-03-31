649eaea122ef23c8da6560c606c5fa42cd4a3055
BTH-IT <hungbienthanh@gmail.com>
2024-03-31 16:01:28 +0700

    add api

--- app/src/main/java/com/instar/frontend_android/ui/services/StoryService.kt
+++ app/src/main/java/com/instar/frontend_android/ui/services/StoryService.kt
@@ -2,11 +2,13 @@ package com.instar.frontend_android.ui.services
 
 import com.instar.frontend_android.types.responses.ApiResponse
 import com.instar.frontend_android.ui.DTO.Story
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface StoryService {
@@ -18,7 +20,7 @@ interface StoryService {
     fun deleteStory(@Path("userId") userId: String, @Path("storyId") storyId: String): Call<ApiResponse<Any>>
 
     @POST("$AUTH_PREFIX/{userId}")
-    fun createStory(@Path("userId") userId: String, @Body storyDetail: Story): Call<ApiResponse<Any>>
+    fun createStory(@Path("userId") userId: String, @Part file: MultipartBody.Part): Call<ApiResponse<Any>>
 
     @GET("$AUTH_PREFIX/{userId}/{storyId}")
     fun getStoryByStoryId(@Path("userId") userId: String, @Path("storyId") storyId: String): Call<ApiResponse<Any>>

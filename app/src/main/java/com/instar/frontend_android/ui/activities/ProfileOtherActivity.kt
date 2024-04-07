7415ba8b34e5243a58cc996170391b23d486c685
tronghieu <tronghieu0944@gmail.com>
2024-04-07 16:09:30 +0700

    show btn in profile other

--- app/src/main/java/com/instar/frontend_android/ui/activities/ProfileOtherActivity.kt
+++ app/src/main/java/com/instar/frontend_android/ui/activities/ProfileOtherActivity.kt
@@ -80,9 +80,9 @@ class ProfileOtherActivity : AppCompatActivity() {
         }
 
         initView()
-        var userOther: User? = intent.getSerializableExtra("userOther") as? User
+        userOther = intent.getSerializableExtra("userOther") as? User
 
-        var user: User? = intent.getSerializableExtra("user") as? User
+        user = intent.getSerializableExtra("user") as? User
 
         myViewPagerAdapter = MyViewPagerAdapter(this@ProfileOtherActivity,userOther?.id)
         viewPager2.adapter = myViewPagerAdapter
@@ -93,15 +93,13 @@ class ProfileOtherActivity : AppCompatActivity() {
         val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
         val accessToken = sharedPreferences.getString("accessToken", null)
 
-        userOther?.id?.let { Log.i("User ID ", it) }
-        user?.id?.let { Log.i("User now ID ", it) }
-        Toast.makeText(this, user?.id , Toast.LENGTH_SHORT).show()
         if (userOther != null) {
-            updateUserOtherInformation(userOther)
+            updateUserOtherInformation(userOther!!)
         }
 
         if (user != null) {
-            updateUserInformation(user)
+            Log.i("RES", user?.followings?.contains(userOther?.id).toString() )
+            updateUserInformation(user!!)
         } else {
             if (accessToken != null) {
                 val decodedTokenJson = Helpers.decodeJwt(accessToken)
@@ -119,7 +117,7 @@ class ProfileOtherActivity : AppCompatActivity() {
             }
         }
 
-        user?.followers
+
     }
     @RequiresApi(Build.VERSION_CODES.R)
     private fun initView() {
@@ -227,7 +225,8 @@ class ProfileOtherActivity : AppCompatActivity() {
             .placeholder(R.drawable.default_image) // Placeholder image
             .error(R.drawable.default_image) // Image to display if load fails
             .into(url)
-        if (user.followings.contains(userOther?.id)) {
+        Log.i("UserOther", userOther?.id.toString())
+        if (user.followings.contains(userOther?.id) == true) {
             btnFollow.visibility = View.GONE
             btnIsFollow.visibility = View.VISIBLE
         } else {
